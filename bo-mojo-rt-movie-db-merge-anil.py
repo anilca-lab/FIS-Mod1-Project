f060bc186cf3203b4f346f5656665f6b64c9ed5e
Anil Onal <anilca@gmail.com>
2019-10-21 10:51:43 -0400

    merges imdb and movie DB files.

--- bo-mojo-rt-movie-db-merge-anil.py
+++ bo-mojo-rt-movie-db-merge-anil.py
@@ -1,22 +1,26 @@
 """
-The code below cleans and merges movie data files from three sources:
-    Box Office Mojo
-    Rotten Tomatoes
+The code below cleans and merges movie data files from two sources:
+    IMDB
     The Movie DB
 """
 
 import pandas as pd
 data_directory = '/Users/flatironschol/fis_projects/Data/'  
-df_bom = pd.read_csv(data_directory+'bom.movie_gross.csv.gz') #Source: Box Office Mojo
-df_rt_movies = pd.read_csv(data_directory+'rt.movie_info.tsv.gz', delimiter = '\t') #Source: Rotten tomatoes
-df_rt_reviews = pd.read_csv(data_directory+'rt.reviews.tsv.gz', delimiter = '\t', encoding = 'cp437') #Source: Rotten tomatoes 
-df_tmdb = pd.read_csv(data_directory+'tmdb.movies.csv.gz') # Source: MovieDB
-df_tn = pd.read_csv(data_directory+'tn.movie_budgets.csv.gz') # Source: MovieDB
+# df_bom = pd.read_csv(data_directory+'bom.movie_gross.csv.gz') #Source: Box Office Mojo
+# df_rt_movies = pd.read_csv(data_directory+'rt.movie_info.tsv.gz', delimiter = '\t') #Source: Rotten tomatoes
+# df_rt_reviews = pd.read_csv(data_directory+'rt.reviews.tsv.gz', delimiter = '\t', encoding = 'cp437') #Source: Rotten tomatoes 
+df_imdb_basics = pd.read_csv(data_directory+'imdb.title.basics.csv.gz') # Source: IMDB
+df_imdb_ratings = pd.read_csv(data_directory+'imdb.title.ratings.csv.gz') # Source: IMDB
+df_mdb_ratings = pd.read_csv(data_directory+'tmdb.movies.csv.gz') # Source: MovieDB
+df_mdb_revenues = pd.read_csv(data_directory+'tn.movie_budgets.csv.gz') # Source: MovieDB
 
 # The function cleans the two Movie DB dataframes
 def df_clean(data_frame, release_date_column, movie_title_column):
     data_frame.drop_duplicates(keep = 'first', inplace = True)
-    data_frame[release_date_column] = pd.to_datetime(data_frame[release_date_column]).dt.to_period("M") # Assume that movies released in the same month of the same year are the same.
+    if data_frame[release_date_column].dtypes == 'int64':
+        data_frame[release_date_column] = pd.to_datetime(data_frame[release_date_column], format = '%Y').dt.to_period("Y") # Assume that movies released in the same month of the same year are the same.
+    else:
+        data_frame[release_date_column] = pd.to_datetime(data_frame[release_date_column]).dt.to_period("Y") # Assume that movies released in the same month of the same year are the same.
     data_frame[movie_title_column] = [" ".join(title.upper().split()) for title in data_frame[movie_title_column]] # Remove whitespaces
     data_frame[movie_title_column] = [title.replace('Â', "'") for title in data_frame[movie_title_column]] 
     data_frame[movie_title_column] = [title.replace('#', '') for title in data_frame[movie_title_column]] 
@@ -26,12 +30,26 @@ def df_clean(data_frame, release_date_column, movie_title_column):
     data_frame.rename(columns={release_date_column: 'release_date', movie_title_column: 'title'}, inplace = True)
     return data_frame
     
-# Merge the two Movie DB dataframes
-df_tmdb.drop('Unnamed: 0', axis = 1, inplace = True) # This is an extra index column
-df_tmdb = df_clean(df_tmdb, 'release_date', 'title')
-df_tn = df_clean(df_tn, 'release_date', 'movie')
-df = pd.merge(df_tmdb, df_tn, how = 'outer', \
+# Merge Movie DB dataframes
+df_mdb_ratings.drop('Unnamed: 0', axis = 1, inplace = True) # This is an extra index column
+df_mdb_ratings = df_clean(df_mdb_ratings, 'release_date', 'title')
+df_mdb_revenues = df_clean(df_mdb_revenues, 'release_date', 'movie')
+df_mdb = pd.merge(df_mdb_ratings, df_mdb_revenues, how = 'outer', \
               left_on = ['title', 'release_date'], \
               right_on = ['title', 'release_date'], \
               indicator = True, validate="one_to_one", \
               sort = True)
+
+# Merge IMDB dataframes
+df_imdb_basics = df_clean(df_imdb_basics, 'start_year', 'primary_title')
+df_imdb = pd.merge(df_imdb_basics, df_imdb_ratings, how = 'outer', \
+              on = 'tconst', \
+              indicator = True, validate="one_to_one")
+
+# Merge Movie DB and IMDB dataframes
+df = pd.merge(df_imdb.drop(columns = '_merge'), df_mdb.drop(columns = '_merge'), how = 'outer', \
+              left_on = ['title', 'release_date'], \
+              right_on = ['title', 'release_date'], \
+              indicator = True, validate="many_to_one", \
+              sort = True)
+df_merged = df.loc[df._merge == 'both']
